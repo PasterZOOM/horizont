3499392c621051f237c3d9b0c7d89bd8b17fab42
ilya <pasterzoom@gmail.com>
2023-07-06 15:10:19 +0300

    feat: add loader fix: query key

--- src/components/pages/products/productsPage.tsx
+++ src/components/pages/products/productsPage.tsx
@@ -4,13 +4,14 @@ import styles from './productsPage.module.scss'
 
 import { Filters } from '@/components/pages/products/filters'
 import { Products } from '@/components/pages/products/products'
+import { Loader } from '@/components/ui/loader/loader.tsx'
 import { Typography } from '@/components/ui/typography'
 import { useGetProducts } from '@/hooks/query/useGetProducts.ts'
 
 export const ProductsPage: FC = () => {
   const { data, isLoading } = useGetProducts()
 
-  if (isLoading) return <>Loading...</>
+  if (isLoading) return <Loader />
   if (!data) return <>Error</>
 
   return (
